ec8260ff68c5ecbd9d89013e4c334357da066bec
Jyunmau <yangyang4910709@163.com>
2019-03-12 00:02:18 +0800

    完成列表页布局和跳转

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,6 +4,7 @@ const app = getApp()
 
 Page({
   data: {
+    colorList: ["bg-gradual-green", "bg-gradual-blue", "bg-gradual-red", "bg-gradual-purple", "bg-gradual-orange", "bg-gradual-pink"],
     activityList: [{
       id: "1234",
       name: "“展风华·为人先”软件开发比赛",
@@ -11,11 +12,47 @@ Page({
       startTime: "2019-03-18 00:00:00",
       endTime: "2019-03-31 00:00:00"
     },{
-      name: "“中兴·图灵杯”人工智能程序设计大赛",
+      id: "2234",
+      name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
       publisher: "院科协",
       startTime: "2019-10-18 00:00:00",
       endTime: "2019-11-18 00:00:00"
-    }]
+      }, {
+        id: "3234",
+        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
+        publisher: "院科协",
+        startTime: "2019-10-18 00:00:00",
+        endTime: "2019-11-18 00:00:00"
+      }, {
+        id: "4234",
+        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
+        publisher: "院科协",
+        startTime: "2019-10-18 00:00:00",
+        endTime: "2019-11-18 00:00:00"
+      }, {
+        id: "5234",
+        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
+        publisher: "院科协",
+        startTime: "2019-10-18 00:00:00",
+        endTime: "2019-11-18 00:00:00"
+      }, {
+        id: "6234",
+        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
+        publisher: "院科协",
+        startTime: "2019-10-18 00:00:00",
+        endTime: "2019-11-18 00:00:00"
+      }],
+    statusBarHeight: app.globalData.statusBarHeight
   },
-  
+  onLoad(){
+  },
+  entryCard(e){
+    var activityID = this.data.activityList[e.currentTarget.id].id
+    var activityName = this.data.activityList[e.currentTarget.id].name
+    var color = this.data.colorList[e.currentTarget.id]
+    console.log(color)
+    wx.navigateTo({
+      url: '../registration/registration?id=' + activityID + '&name=' + activityName + '&color=' + color,
+    })
+  }
 })
--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,66 +1,27 @@
 // pages/registration/registration.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    title: "返回",
+    navColor: "",
+    statusBarHeight: app.globalData.statusBarHeight
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    console.log(options.id)
+    this.setData({
+      title: options.name
+    })
+    this.setData({
+      navColor: options.color
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#000000'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  back2list(){
+    wx.navigateBack({
+      
+    })
   }
 })
