10f8ab3f88091173c8a136106d37333c4107aa51
Jyunmau <yangyang4910709@163.com>
2019-03-16 10:57:54 +0800

    完成接收&展示表单

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,50 +1,17 @@
 //index.js
+import regeneratorRuntime from '../../regenerator-runtime/runtime.js';
+
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
     colorList: ["bg-gradual-green", "bg-gradual-blue", "bg-gradual-red", "bg-gradual-purple", "bg-gradual-orange", "bg-gradual-pink"],
-    activityList: [{
-        id: "1234",
-        name: "“展风华·为人先”软件开发比赛",
-        publisher: "院科协",
-        startTime: "2019-03-18 00:00:00",
-        endTime: "2019-03-31 00:00:00"
-      },{
-        id: "2234",
-        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
-        publisher: "院科协",
-        startTime: "2019-10-18 00:00:00",
-        endTime: "2019-11-18 00:00:00"
-      }, {
-        id: "3234",
-        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
-        publisher: "院科协",
-        startTime: "2019-10-18 00:00:00",
-        endTime: "2019-11-18 00:00:00"
-      }, {
-        id: "4234",
-        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
-        publisher: "院科协",
-        startTime: "2019-10-18 00:00:00",
-        endTime: "2019-11-18 00:00:00"
-      }, {
-        id: "5234",
-        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
-        publisher: "院科协",
-        startTime: "2019-10-18 00:00:00",
-        endTime: "2019-11-18 00:00:00"
-      }, {
-        id: "6234",
-        name: "“中兴·图灵杯”人工智能程序设计大赛啊哈哈哈哈哈哈哈哈",
-        publisher: "院科协",
-        startTime: "2019-10-18 00:00:00",
-        endTime: "2019-11-18 00:00:00"
-      }],
+    activityList: [],
     statusBarHeight: app.globalData.statusBarHeight
   },
   onLoad(){
+    this.getActivity();
   },
   entryCard(e){
     var activityID = this.data.activityList[e.currentTarget.id].id
@@ -53,5 +20,24 @@ Page({
     wx.navigateTo({
       url: '../registration/registration?id=' + activityID + '&name=' + activityName + '&color=' + color,
     })
-  }
+  },
+  async getActivity() {
+    let res = await new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://www.turing-cup.online/voteapp/activity',
+        method: 'GET',
+        success: ({ data }) => {
+          resolve(data)
+        },
+        fail: reject
+      })
+    })
+    if (res) {
+      this.setData({
+        activityList: res
+      })
+    } else {
+      console.log("没有成功")
+    }
+  },
 })
--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,4 +1,5 @@
 // pages/registration/registration.js
+import regeneratorRuntime from '../../regenerator-runtime/runtime.js';
 const app = getApp()
 Page({
   data: {
@@ -6,72 +7,10 @@ Page({
     navColor: "",
     statusBarHeight: app.globalData.statusBarHeight,
     id: "",
-    regsList: [
-      {
-        name: "队名",
-        type: "text",
-        defaultValue: "请输入队名",
-        description: "仅允许输入中文，不超过5个字",
-        tip: "仅支持中文，不超过5个字",
-        require: true,
-        range: [1, 5]
-      },
-      {
-        name: "组员一信息",
-        type: "group",
-        description: "组员一的个人信息",
-        tip: "请输入组员信息",
-        require: true,
-        subItem: [
-          {
-            name: "姓名",
-            type: "text",
-            description: "仅支持中文，不超过5个字",
-            tip: "仅支持中文，不超过5个字",
-            require: true,
-            range: [1, 5]
-          },
-          {
-            name: "性别",
-            type: "sex",
-            defaultValue: "男",
-            description: "请选择你的性别",
-            tip: "请输入性别",
-            require: true,
-          },
-          {
-            name: "出生日期",
-            type: "date",
-            defaultValue: "1999-01-01",
-            description: "请选择你的出生年月",
-            tip: "请选择你的出生年月",
-            require: true,
-          },
-          {
-            name: "目标岗位",
-            type: "checkbox",
-            description: "*请至少选择一组，最多2组",
-            tip: "岗位",
-            require: false,
-            case: ["宣传部", "技术部", "美工部"],
-            range: [1, 2]
-          },
-          {
-            name: "任职设想",
-            type: "txtarea",
-            description: "描述你对职业的设想",
-            tip: "请在这里写下职业设想...",
-            require: false,
-            range: [1, 5]
-          }
-        ]
-      }
-    ]
+    regsList: []
   },
   onLoad: function (options) {
-    this.setData({
-      title: options.id
-    })
+    this.getRegistration(options.id)
     this.setData({
       title: options.name
     })
@@ -82,6 +21,27 @@ Page({
       frontColor: '#ffffff',
       backgroundColor: '#000000'
     })
+  },
+  async getRegistration(id) {
+    let res = await new Promise((resolve, reject) => {
+      wx.request({
+        url: 'https://www.turing-cup.online/voteapp/activity/'+id,
+        method: 'GET',
+        success: ({ data }) => {
+          resolve(data)
+        },
+        fail: reject
+      })
+    })
+    if (res) {
+      this.setData({
+        regsList: res
+      })
+      console.log(this.data.regsList)
+    } else {
+      console.log("没有成功")
+      // 未获取成功
+    }
   },
   back2list(){
     wx.navigateBack({
